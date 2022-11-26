1e205144ac065a00706c307cda577ebb36238fd6
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-11-26 22:26:19 +0100

    Pawn - init

---
+++ pawn.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "coordinates.hpp" // Coord, Coordinates, <utility>
+#include "ANSI-Settings.hpp" // ANSI::ForegroundColor, ANSI::BackgroundColor, ANSI::Attribute, ANSI::Settings
+
+
+class Pawn { // Pawn class - represents an object on the field [parent class]
+public:
+    Coordinates coordinates; // Coordinates of the pawn
+    ANSI::Settings settings; // settings of the pawn style
+    char symbol; // Symbol of the pawn
+
+    Pawn(Coordinates coordinates_, char symbol_, ANSI::Settings settings_): coordinates(coordinates_), symbol(symbol_), settings(settings_) {} // Constructor
+    Pawn(Coordinates& coordinates_, char symbol_, ANSI::Settings& settings_): coordinates(coordinates_), symbol(symbol_), settings(settings_) {} // Constructor
+
+    virtual void print() { // Print the pawn
+        settings.apply(); // Apply the settings
+        std::cout << symbol; // Print the symbol
+    }
+};
