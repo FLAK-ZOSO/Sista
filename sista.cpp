1c2160efae52798c0200ded4d7cb5d973179acc3
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-11-27 22:11:37 +0100

    Refresh testing
    Need # border for field

--- sista.cpp
+++ sista.cpp
@@ -1,8 +1,12 @@
 // g++ sista.cpp -o sista
+#include <chrono>
+#include <thread>
 #include "field.hpp"
 
 
 int main() {
+    ANSI::reset(); // Reset the settings
+    std::cout << HIDE_CURSOR << CLS;
     Pawn pawn(
         Coordinates(5, 4), 'X',
         ANSI::Settings(
@@ -14,5 +18,10 @@ int main() {
     Field field(10, 10);
     field.addPawn(&pawn);
     field.print();
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::cout << CLS;
+    field.movePawn(&pawn, 2, 2);
+    field.print();
+    std::cout << SHOW_CURSOR;
     return 0;
 }
