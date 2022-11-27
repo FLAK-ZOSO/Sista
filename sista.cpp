482dc1670129c5d7fb17885f56b63d81a414a8d5
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-11-27 22:01:04 +0100

    Field testing

--- sista.cpp
+++ sista.cpp
@@ -1,17 +1,18 @@
 // g++ sista.cpp -o sista
-#include "pawn.hpp"
+#include "field.hpp"
 
 
 int main() {
     Pawn pawn(
-        Coordinates(0, 0), 'X',
+        Coordinates(5, 4), 'X',
         ANSI::Settings(
             ANSI::ForegroundColor::F_RED,
             ANSI::BackgroundColor::B_BLACK,
             ANSI::Attribute::BRIGHT
         )
     );
-    pawn.print();
-    ANSI::reset();
+    Field field(10, 10);
+    field.addPawn(&pawn);
+    field.print();
     return 0;
 }
