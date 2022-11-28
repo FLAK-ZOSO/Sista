3765a5ca39d667181a10a711f18cc22553ff3814
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-11-28 15:35:10 +0100

    Border
    Border - represents a brick of the border

---
+++ border.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "coordinates.hpp" // Coord, Coordinates, <utility>
+#include "ANSI-Settings.hpp" // ANSI::ForegroundColor, ANSI::BackgroundColor, ANSI::Attribute, ANSI::Settings
+
+
+class Border { // Border class - represents a brick of the border [parent class]
+public:
+    ANSI::Settings settings; // settings of the Border style
+    char symbol; // Symbol of the Border
+
+    Border(char symbol_, ANSI::Settings settings_): symbol(symbol_), settings(settings_) {} // Constructor
+    Border(char symbol_, ANSI::Settings& settings_): symbol(symbol_), settings(settings_) {} // Constructor
+
+    virtual void print() { // Print the Border
+        settings.apply(); // Apply the settings
+        std::cout << symbol; // Print the symbol
+    }
+};
