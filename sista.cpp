a896728b2373d4beace109b1ff2d0974069f2029
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-11-28 15:38:37 +0100

    Sista testing - Field(10, 10);
    All seems to work, even if very slowly.

--- sista.cpp
+++ sista.cpp
@@ -5,23 +5,37 @@
 
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
     ANSI::reset(); // Reset the settings
-    std::cout << HIDE_CURSOR << CLS;
+    std::cout << HIDE_CURSOR << SS;
     Pawn pawn(
-        Coordinates(5, 4), 'X',
+        Coordinates(0, 0), 'X',
         ANSI::Settings(
             ANSI::ForegroundColor::F_RED,
-            ANSI::BackgroundColor::B_BLUE,
+            ANSI::BackgroundColor::B_BLACK,
+            ANSI::Attribute::BRIGHT
+        )
+    );
+    Border border(
+        '#', ANSI::Settings(
+            ANSI::ForegroundColor::F_BLACK,
+            ANSI::BackgroundColor::B_WHITE,
             ANSI::Attribute::BRIGHT
         )
     );
     Field field(10, 10);
     field.addPawn(&pawn);
-    field.print();
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::cout << CLS;
-    field.movePawn(&pawn, 2, 2);
-    field.print();
+    for (int i=0; i<10; i++) {
+        for (int j=0; j<10; j++) {
+            field.movePawn(&pawn, Coordinates(i, j));
+            std::this_thread::sleep_for(std::chrono::milliseconds(30));
+            std::cout << SS;
+            std::cout << "\x1b[1000A"; // Move the cursor up 1000 lines
+            field.print(border);
+        }
+    }
+    std::cout << SS;
+    field.print(border);
     std::cout << SHOW_CURSOR;
     return 0;
 }
