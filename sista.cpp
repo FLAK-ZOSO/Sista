b306f553bb50294e48866b6f240bb26d377d1d7f
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-12-22 23:07:12 +0100

    Sista testing - PacMan Effect

--- sista.cpp
+++ sista.cpp
@@ -3,24 +3,14 @@
 #include <thread>
 #include "include/sista/sista.hpp"
 
-#define TEST_SIZE 50
-
-
-class Character: public Pawn {
-public:
-    Character(char symbol_, Coordinates coordinates_, ANSI::Settings settings_): Pawn(symbol_, coordinates_, settings_) {}
-
-    void print() override {
-        Pawn::print();
-    }
-};
+#define TEST_SIZE 10
 
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     ANSI::reset(); // Reset the settings
     std::cout << HIDE_CURSOR << CLS;
-    Character pawn(
+    Pawn pawn(
         'X', Coordinates(0, 0),
         ANSI::Settings(
             ANSI::ForegroundColor::F_RED,
@@ -49,8 +39,8 @@ int main() {
     Coordinates coords(0, 0);
     for (int i=0; i<TEST_SIZE; i++) {
         for (int j=0; j<TEST_SIZE; j++) {
-            field.movePawnBy(&pawn, 0, 1, MATRIX_EFFECT);
-            field.movePawnBy(&pawn2, 0, -1, MATRIX_EFFECT);
+            field.movePawnBy(&pawn, 0, 1, PACMAN_EFFECT);
+            field.movePawnBy(&pawn2, 0, -1, PACMAN_EFFECT);
 
             std::this_thread::sleep_for(std::chrono::milliseconds(20));
             clearScreen();
