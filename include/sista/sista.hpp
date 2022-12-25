d0ce241d311800aa24913647614983e57dbf729b
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-12-25 18:53:09 +0100

    PACMAN_EFFECT - Working
    See debug.txt, there were several idiotic errors in the code.

--- include/sista/sista.hpp
+++ include/sista/sista.hpp
@@ -3,8 +3,9 @@
 #include "ANSI-Settings.hpp" // ANSI::ForegroundColor, ANSI::BackgroundColor, ANSI::Attribute, ANSI::Settings
 #include "border.hpp" // Border
 #include "coordinates.hpp" // Coord, Coordinates, <utility>
-#include "field.hpp" // Field
 #include "pawn.hpp" // Pawn
+#include "field.hpp" // Field
+#include "SwappableField.hpp" // Path, SwappableField
 #if (_WIN32 || _WIN64)
     #include "clearScreen.hpp" // clearScreen [Windows-specific]
 #endif
--- sista.cpp
+++ sista.cpp
@@ -1,4 +1,4 @@
-// g++ sista.cpp -o sista
+// g++ sista.cpp -o sista -std=c++17 -Wall -Wextra
 #include <chrono>
 #include <thread>
 #include "include/sista/sista.hpp"
@@ -9,7 +9,8 @@
 int main() {
     std::ios_base::sync_with_stdio(false);
     ANSI::reset(); // Reset the settings
-    std::cout << HIDE_CURSOR << CLS;
+    std::cout << HIDE_CURSOR;
+    clearScreen();
     Pawn pawn(
         'X', Coordinates(0, 0),
         ANSI::Settings(
@@ -33,22 +34,36 @@ int main() {
             ANSI::Attribute::BRIGHT
         )
     );
-    Field field(TEST_SIZE, TEST_SIZE);
+    SwappableField field(TEST_SIZE, TEST_SIZE);
     field.addPawn(&pawn);
     field.addPawn(&pawn2);
     Coordinates coords(0, 0);
     for (int i=0; i<TEST_SIZE; i++) {
         for (int j=0; j<TEST_SIZE; j++) {
-            field.movePawnBy(&pawn, 0, 1, PACMAN_EFFECT);
-            field.movePawnBy(&pawn2, 0, -1, PACMAN_EFFECT);
+            debug << "Calculating arrival coordinates for first pawn." << std::endl;
+            coords = field.movingByCoordinates(&pawn, 0, 1, PACMAN_EFFECT);
+
+            debug << "Querying swap for first pawn from (" << pawn.getCoordinates().y << ", " << pawn.getCoordinates().x << ") to (" << coords.y << ", " << coords.x << ")" << std::endl;
+            field.addPawnToSwap(&pawn, coords);
+
+            debug << "Calculating arrival coordinates for second pawn." << std::endl;
+            coords = field.movingByCoordinates(&pawn2, 0, -1, PACMAN_EFFECT);
+
+            debug << "Querying swap for second pawn from (" << pawn2.getCoordinates().y << ", " << pawn2.getCoordinates().x << ") to (" << coords.y << ", " << coords.x << ")" << std::endl;
+            field.addPawnToSwap(&pawn2, coords);
+
+            field.applySwaps();
+
+            // field.movePawnBy(&pawn, 0, 1, PACMAN_EFFECT);
+            // field.movePawnBy(&pawn2, 0, -1, PACMAN_EFFECT);
 
             std::this_thread::sleep_for(std::chrono::milliseconds(20));
             clearScreen();
             field.print(border);
         }
     }
-    std::cout << CLS;
-    field.print(border);
+    clearScreen();
     std::cout << SHOW_CURSOR;
+    field.print(border);
     return 0;
 }
