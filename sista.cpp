a00ffbbc02ae54c95111fc3b14c27985de2aedfa
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-12-26 23:39:54 +0100

    30x30 Sista testing

--- sista.cpp
+++ sista.cpp
@@ -3,7 +3,7 @@
 #include <thread>
 #include "include/sista/sista.hpp"
 
-#define TEST_SIZE 10
+#define TEST_SIZE 30
 
 
 int main() {
@@ -20,34 +20,58 @@ int main() {
         )
     );
     sista::Pawn* pawn2 = new sista::Pawn(
-        'O', sista::Coordinates(TEST_SIZE-1, TEST_SIZE-1),
+        'O', sista::Coordinates(TEST_SIZE*0.2, TEST_SIZE*0.7),
         ANSI::Settings(
             ANSI::ForegroundColor::F_BLUE,
             ANSI::BackgroundColor::B_BLACK,
             ANSI::Attribute::BRIGHT
         )
     );
+    sista::Pawn* pawn3 = new sista::Pawn(
+        '@', sista::Coordinates(TEST_SIZE*0.3, 0),
+        ANSI::Settings(
+            ANSI::ForegroundColor::F_MAGENTA,
+            ANSI::BackgroundColor::B_YELLOW,
+            ANSI::Attribute::REVERSE
+        )
+    );
+    sista::Pawn* pawn4 = new sista::Pawn(
+        '%', sista::Coordinates(0, TEST_SIZE*0.5),
+        ANSI::Settings(
+            ANSI::ForegroundColor::F_YELLOW,
+            ANSI::BackgroundColor::B_MAGENTA,
+            ANSI::Attribute::REVERSE
+        )
+    );
     sista::Border border(
-        ' ', ANSI::Settings(
-            ANSI::ForegroundColor::F_BLACK,
-            ANSI::BackgroundColor::B_WHITE,
+        '#', ANSI::Settings(
+            ANSI::ForegroundColor::F_WHITE,
+            ANSI::BackgroundColor::B_BLACK,
             ANSI::Attribute::BRIGHT
         )
     );
     sista::SwappableField field(TEST_SIZE, TEST_SIZE);
     field.addPawn(pawn);
     field.addPawn(pawn2);
-    sista::Coordinates coords(0, 0), coords2(0, 0);
+    field.addPawn(pawn3);
+    field.addPawn(pawn4);
+    sista::Coordinates coords, coords2, coords3, coords4;
     for (int i=0; i<TEST_SIZE; i++) {
         for (int j=0; j<TEST_SIZE; j++) {
-            coords = field.movingByCoordinates(pawn, 0, 1, MATRIX_EFFECT);
-            coords2 = field.movingByCoordinates(pawn2, 0, -1, MATRIX_EFFECT);
+            coords = field.movingByCoordinates(pawn, 1, 1, PACMAN_EFFECT);
+            coords2 = field.movingByCoordinates(pawn2, -1, -1, PACMAN_EFFECT);
+            coords3 = field.movingByCoordinates(pawn3, -1, 1, PACMAN_EFFECT);
+            coords4 = field.movingByCoordinates(pawn4, 1, -1, PACMAN_EFFECT);
             try {
                 field.movePawn(pawn, coords);
                 field.movePawn(pawn2, coords2);
+                field.movePawn(pawn3, coords3);
+                field.movePawn(pawn4, coords4);
             } catch (const std::invalid_argument& e) {
                 field.addPawnToSwap(pawn, coords);
                 field.addPawnToSwap(pawn2, coords2);
+                field.addPawnToSwap(pawn3, coords3);
+                field.addPawnToSwap(pawn4, coords4);
                 field.applySwaps();
             }
 
