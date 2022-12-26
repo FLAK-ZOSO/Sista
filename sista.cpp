25c8dbd5128cda51f795a1f4931d695eb275ca2b
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-12-26 23:54:19 +0100

    Sista.cpp - More scalable code.

--- sista.cpp
+++ sista.cpp
@@ -11,36 +11,46 @@ int main() {
     ANSI::reset(); // Reset the settings
     std::cout << HIDE_CURSOR;
     clearScreen();
-    sista::Pawn* pawn = new sista::Pawn(
-        'X', sista::Coordinates(0, 0),
-        ANSI::Settings(
-            ANSI::ForegroundColor::F_RED,
-            ANSI::BackgroundColor::B_BLACK,
-            ANSI::Attribute::BRIGHT
+
+    std::vector<sista::Pawn*> pawns;
+    pawns.push_back(
+        new sista::Pawn(
+            'X', sista::Coordinates(0, 0),
+            ANSI::Settings(
+                ANSI::ForegroundColor::F_RED,
+                ANSI::BackgroundColor::B_BLACK,
+                ANSI::Attribute::BRIGHT
+            )
         )
     );
-    sista::Pawn* pawn2 = new sista::Pawn(
-        'O', sista::Coordinates(TEST_SIZE*0.2, TEST_SIZE*0.7),
-        ANSI::Settings(
-            ANSI::ForegroundColor::F_BLUE,
-            ANSI::BackgroundColor::B_BLACK,
-            ANSI::Attribute::BRIGHT
+    pawns.push_back(
+        new sista::Pawn(
+            'O', sista::Coordinates(TEST_SIZE*0.2, TEST_SIZE*0.7),
+            ANSI::Settings(
+                ANSI::ForegroundColor::F_BLUE,
+                ANSI::BackgroundColor::B_BLACK,
+                ANSI::Attribute::BRIGHT
+            )
         )
     );
-    sista::Pawn* pawn3 = new sista::Pawn(
-        '@', sista::Coordinates(TEST_SIZE*0.3, 0),
-        ANSI::Settings(
-            ANSI::ForegroundColor::F_MAGENTA,
-            ANSI::BackgroundColor::B_YELLOW,
-            ANSI::Attribute::REVERSE
+    pawns.push_back(
+        new sista::Pawn(
+            '@', sista::Coordinates(TEST_SIZE*0.3, 0),
+            ANSI::Settings(
+                ANSI::ForegroundColor::F_MAGENTA,
+                ANSI::BackgroundColor::B_YELLOW,
+                ANSI::Attribute::REVERSE
+            )
         )
     );
-    sista::Pawn* pawn4 = new sista::Pawn(
-        '%', sista::Coordinates(0, TEST_SIZE*0.5),
-        ANSI::Settings(
-            ANSI::ForegroundColor::F_YELLOW,
-            ANSI::BackgroundColor::B_MAGENTA,
-            ANSI::Attribute::REVERSE
+    pawns.push_back(
+        new sista::Pawn(
+            '%', sista::Coordinates(TEST_SIZE*0.5, TEST_SIZE*0.5),
+            ANSI::Settings(
+                ANSI::ForegroundColor::F_YELLOW,
+                ANSI::BackgroundColor::B_MAGENTA,
+                ANSI::Attribute::REVERSE
+            )
         )
     );
     sista::Border border(
@@ -51,27 +61,24 @@ int main() {
         )
     );
     sista::SwappableField field(TEST_SIZE, TEST_SIZE);
-    field.addPawn(pawn);
-    field.addPawn(pawn2);
-    field.addPawn(pawn3);
-    field.addPawn(pawn4);
-    sista::Coordinates coords, coords2, coords3, coords4;
+    for (auto pawn : pawns) {
+        field.addPawn(pawn);
+    }
+    std::vector<sista::Coordinates> coords(4);
     for (int i=0; i<TEST_SIZE; i++) {
         for (int j=0; j<TEST_SIZE; j++) {
-            coords = field.movingByCoordinates(pawn, 1, 1, PACMAN_EFFECT);
-            coords2 = field.movingByCoordinates(pawn2, -1, -1, PACMAN_EFFECT);
-            coords3 = field.movingByCoordinates(pawn3, -1, 1, PACMAN_EFFECT);
-            coords4 = field.movingByCoordinates(pawn4, 1, -1, PACMAN_EFFECT);
+            coords[0] = field.movingByCoordinates(pawns[0], 1, 1, PACMAN_EFFECT);
+            coords[1] = field.movingByCoordinates(pawns[1], -1, -1, PACMAN_EFFECT);
+            coords[2] = field.movingByCoordinates(pawns[2], -1, 1, PACMAN_EFFECT);
+            coords[3] = field.movingByCoordinates(pawns[3], 1, -1, PACMAN_EFFECT);
             try {
-                field.movePawn(pawn, coords);
-                field.movePawn(pawn2, coords2);
-                field.movePawn(pawn3, coords3);
-                field.movePawn(pawn4, coords4);
+                for (int k=0; k<4; k++) {
+                    field.movePawn(pawns[k], coords[k]);
+                }
             } catch (const std::invalid_argument& e) {
-                field.addPawnToSwap(pawn, coords);
-                field.addPawnToSwap(pawn2, coords2);
-                field.addPawnToSwap(pawn3, coords3);
-                field.addPawnToSwap(pawn4, coords4);
+                for (int k=0; k<4; k++) {
+                    field.addPawnToSwap(pawns[k], coords[k]);
+                }
                 field.applySwaps();
             }
 
