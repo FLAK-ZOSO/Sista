56cce410403099a1c9cf05588da4237ac0c3a37d
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-12-28 15:19:34 +0100

    clearScreen.hpp - Cross-platform

--- include/sista/clearScreen.hpp
+++ include/sista/clearScreen.hpp
@@ -1,44 +1,9 @@
 #pragma once
 
-#include <windows.h>
-
-
-void clearScreen(bool spaces=true, bool attributes=false) {
-    HANDLE                     hStdOut;
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    DWORD                      count;
-    DWORD                      cellCount;
-    COORD                      homeCoords = { 0, 0 };
-
-    hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
-    if (hStdOut == INVALID_HANDLE_VALUE) return;
-
-    /* Get the number of cells in the current buffer */
-    if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
-    cellCount = csbi.dwSize.X *csbi.dwSize.Y;
-
+void clearScreen(bool spaces=true) {
     if (spaces) {
-        /* Fill the entire buffer with spaces */
-        if (!FillConsoleOutputCharacter(
-            hStdOut,
-            (TCHAR) ' ',
-            cellCount,
-            homeCoords,
-            &count
-        )) return;
+        std::cout << CSI_ << "2J"; // Clear screen
+        std::cout << "\x1b[3J"; // Clear scrollback buffer
     }
-
-    if (attributes) {
-        /* Fill the entire buffer with the current colors and attributes */
-        if (!FillConsoleOutputAttribute(
-            hStdOut,
-            csbi.wAttributes,
-            cellCount,
-            homeCoords,
-            &count
-        )) return;
-    }
-
-    /* Move the cursor home */
-    SetConsoleCursorPosition( hStdOut, homeCoords );
+    std::cout << "\x1b[0;0H"; // Move cursor to top-left corner
 }
