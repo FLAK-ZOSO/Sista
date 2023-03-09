56408840f7aa2048150536ecf1fd2a630c852364
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2023-03-09 17:46:35 +0100

    Demo - Colors 24-bit

---
+++ demo/colors24-bit.cpp
@@ -0,0 +1,14 @@
+#include "../include/sista/sista.hpp"
+
+int main() {
+    for (int r = 0; r < 256; r += 16) {
+        for (int g = 0; g < 256; g += 16) {
+            for (int b = 0; b < 256; b += 16) {
+                ANSI::setBackgroundColor(r, g, b);
+                std::cout << ' ';
+                ANSI::reset();
+            }
+            std::cout << '\n';
+        }
+    }
+}
