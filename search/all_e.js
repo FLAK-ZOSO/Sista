29ba0911ef904556eb00a7e78fe9ce6c6ccb7e3f
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2025-08-24 12:13:03 +0000

    Deploying to gh-pages from @ FLAK-ZOSO/Sista@0ebba908b0fe2883e4f3208ba57004f17ee5aba7 🚀

--- search/all_e.js
+++ search/all_e.js
@@ -6,12 +6,11 @@ var searchData=
   ['red_3',['RED',['../namespacesista.html#a72a9a13a3fd5aa83f5852d073bcdbe28aa2d9547b5d3dd9f05984475f7c926da0',1,'sista::RED()'],['../namespacesista.html#ade2b5834bc07e347f46027a10a06d1caaa2d9547b5d3dd9f05984475f7c926da0',1,'sista::RED()']]],
   ['removepawn_4',['removePawn',['../classsista_1_1Field.html#ab1d0a24261d8e595ff5f719fd6e28d5c',1,'sista::Field::removePawn(Pawn *)'],['../classsista_1_1Field.html#a57145101adecb9c9855df01f69e5c167',1,'sista::Field::removePawn(const Coordinates &amp;)'],['../classsista_1_1SwappableField.html#abfe055739090c33966a736be1c020b11',1,'sista::SwappableField::removePawn()']]],
   ['reprintpawn_5',['rePrintPawn',['../classsista_1_1Field.html#af3ee2d7fc112ba03a697f4fe5ae2ea7e',1,'sista::Field']]],
-  ['reset_6',['reset',['../classsista_1_1Field.html#a961b2f1f944fa4d11f23ed94b858241a',1,'sista::Field']]],
-  ['reset_7',['RESET',['../namespacesista.html#a429fa0f15cf3706b0efb6c28f9017d2fab5859d8721cfdc0312b2838b9c985bc1',1,'sista']]],
-  ['resetansi_8',['resetAnsi',['../namespacesista.html#a01b6f49cb4ab2bca5aa726b576a6f3ad',1,'sista']]],
-  ['resetattribute_9',['resetAttribute',['../namespacesista.html#a175e0b48e9f62f849e02dc4cefdd394a',1,'sista']]],
-  ['restore_5fcursor_5fposition_10',['RESTORE_CURSOR_POSITION',['../namespacesista.html#af3bc6f60c5d75fa9fa275946c516ee51a62684cb7aa31e0d521228313ef65bb7b',1,'sista']]],
-  ['reverse_11',['REVERSE',['../namespacesista.html#a429fa0f15cf3706b0efb6c28f9017d2fa642e0b6684e6165e142c074f1cd8d55c',1,'sista']]],
-  ['rgbcolor_12',['RGBColor',['../structsista_1_1RGBColor.html#ad55ce7ba7ce6fbcaa78f9c08a956732c',1,'sista::RGBColor::RGBColor(unsigned short int, unsigned short int, unsigned short int)'],['../structsista_1_1RGBColor.html#a6fefbc7d8ec4427f2a783bf7e89c24fe',1,'sista::RGBColor::RGBColor()'],['../structsista_1_1RGBColor.html',1,'sista::RGBColor']]],
-  ['right_13',['RIGHT',['../namespacesista.html#a7aced27e09de586e7565fcad8f5459e4a21507b40c80068eda19865706fdc2403',1,'sista']]]
+  ['reset_6',['RESET',['../namespacesista.html#a429fa0f15cf3706b0efb6c28f9017d2fab5859d8721cfdc0312b2838b9c985bc1',1,'sista']]],
+  ['resetansi_7',['resetAnsi',['../namespacesista.html#a01b6f49cb4ab2bca5aa726b576a6f3ad',1,'sista']]],
+  ['resetattribute_8',['resetAttribute',['../namespacesista.html#a175e0b48e9f62f849e02dc4cefdd394a',1,'sista']]],
+  ['restore_5fcursor_5fposition_9',['RESTORE_CURSOR_POSITION',['../namespacesista.html#af3bc6f60c5d75fa9fa275946c516ee51a62684cb7aa31e0d521228313ef65bb7b',1,'sista']]],
+  ['reverse_10',['REVERSE',['../namespacesista.html#a429fa0f15cf3706b0efb6c28f9017d2fa642e0b6684e6165e142c074f1cd8d55c',1,'sista']]],
+  ['rgbcolor_11',['RGBColor',['../structsista_1_1RGBColor.html#a6fefbc7d8ec4427f2a783bf7e89c24fe',1,'sista::RGBColor::RGBColor()'],['../structsista_1_1RGBColor.html#ad55ce7ba7ce6fbcaa78f9c08a956732c',1,'sista::RGBColor::RGBColor(unsigned short int, unsigned short int, unsigned short int)'],['../structsista_1_1RGBColor.html',1,'sista::RGBColor']]],
+  ['right_12',['RIGHT',['../namespacesista.html#a7aced27e09de586e7565fcad8f5459e4a21507b40c80068eda19865706fdc2403',1,'sista']]]
 ];
