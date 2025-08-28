aff1645aa1858cc7acfdc5dd63583272bd45be02
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2025-08-29 00:34:07 +0300

    C API - Python FFI test, ctypes needs symbols from libSista for using libSista_api, maybe

--- demo/ffi-test.py
+++ demo/ffi-test.py
@@ -10,8 +10,8 @@ except (OSError, FileNotFoundError):
         ctypes.cdll.LoadLibrary('libSista_api.dylib')
         sista_api = ctypes.CDLL('libSista_api.dylib')
     except (OSError, FileNotFoundError):
-        ctypes.cdll.LoadLibrary(r'C:\\Program Files\Sista\lib\libSista_api.dll')
-        sista_api = ctypes.CDLL(r'C:\\Program Files\Sista\lib\libSista_api.dll')
+        ctypes.windll.LoadLibrary(r'C:\\Program Files\Sista\lib\libSista_api.dll')
+        sista_api = ctypes.WinDLL(r'C:\\Program Files\Sista\lib\libSista_api.dll')
 
 class sista_SwappableField(ctypes.Structure):
     ...
