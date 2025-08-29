bdda35568ca959fb8be1bb8756a5691fe8665732
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2025-08-29 23:07:06 +0300

    C API - Python FFI test, improved code and decided to use only C output stream

--- demo/ffi-test.py
+++ demo/ffi-test.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python3
-import ctypes
+import ctypes, ctypes.util
+import platform
 import time
 import os
 
-try:
-    ctypes.cdll.LoadLibrary('libSista_api.so')
-    sista_api = ctypes.CDLL('libSista_api.so')
-except (OSError, FileNotFoundError):
-    try:
-        ctypes.cdll.LoadLibrary('libSista_api.dylib')
-        sista_api = ctypes.CDLL('libSista_api.dylib')
-    except (OSError, FileNotFoundError):
+match platform.system():
+    case "Linux":
+        sista_api_lib_path = ctypes.util.find_library("Sista_api")
+        ctypes.cdll.LoadLibrary(sista_api_lib_path)
+        sista_api = ctypes.CDLL(sista_api_lib_path)
+    case "Darwin":
+        sista_api_lib_path = ctypes.util.find_library("Sista_api")
+        ctypes.cdll.LoadLibrary(sista_api_lib_path)
+        sista_api = ctypes.CDLL(sista_api_lib_path)
+    case "Windows":
         print(os.environ["PATH"])
-        ctypes.windll.LoadLibrary(r'C:\Program Files\Sista\lib\libSista.dll')
-        sista = ctypes.WinDLL(r'C:\Program Files\Sista\lib\libSista.dll')
-        ctypes.windll.LoadLibrary(r'C:\Program Files\Sista\lib\libSista_api.dll')
-        sista_api = ctypes.WinDLL(r'C:\Program Files\Sista\lib\libSista_api.dll')
+        sista_lib_path = ctypes.util.find_library("Sista")
+        ctypes.windll.LoadLibrary(sista_lib_path)
+        sista = ctypes.WinDLL(sista_lib_path)
+        sista_api_lib_path = ctypes.util.find_library("Sista_api")
+        ctypes.windll.LoadLibrary(sista_api_lib_path)
+        sista_api = ctypes.WinDLL(sista_api_lib_path)
+
+match platform.system():
+    case "Windows":
+        libc_path = ctypes.util.find_library("msvcrt")
+        ctypes.windll.LoadLibrary(libc_path)
+        libc = ctypes.WinDLL(libc_path)
+    case "Darwin" | "Linux":
+        libc_path = ctypes.util.find_library("c")
+        ctypes.cdll.LoadLibrary(libc_path)
+        libc = ctypes.CDLL(libc_path)
+
+sista_api.sista_setForegroundColor.argtypes = [ctypes.c_int]
+sista_api.sista_setForegroundColor.restype = None
 
 class sista_SwappableField(ctypes.Structure):
     ...
@@ -30,10 +48,16 @@ field = sista_api.sista_createSwappableField(
     ctypes.c_size_t(42),
     ctypes.c_size_t(42//2)
 )
-print(field)
+libc.puts(str(field).encode())
 time.sleep(2)
 if (field != ctypes.c_size_t(0)):
     sista_api.sista_printField(field, ctypes.c_char(b'#'))
 
 time.sleep(2)
 sista_api.sista_destroySwappableField(field)
+
+sista_api.sista_setForegroundColor(31)
+libc.puts(b"This text is in red, foreground color number 31 for ANSI")
+sista_api.sista_resetAnsi()
+libc.puts(b"This text instead is in the default ANSI style settings")
+time.sleep(1)
