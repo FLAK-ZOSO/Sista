0d0d04f3c55db4eef5292ac086feb6932628a8c6
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2025-08-30 22:07:28 +0300

    C API - Valgrind had revealed definitely lost: 60 bytes in 3 blocks

--- demo/api-test-swap.cpp
+++ demo/api-test-swap.cpp
@@ -3,43 +3,75 @@
 #include <chrono>
 #include <thread>
 
-
+/* When reading this file, consider that checking for nullptr each time can be
+*  omitted for clarity, as it is just defensive programming... a failed allocation
+*  would be very rare in a simple demo like this.
+*/ 
 int main() {
     SwappableFieldHandler_t field = sista_createSwappableField(10, 10);
     if (field == nullptr) {
         return 1; // Could not allocate memory for the field
     }
+    ANSISettingsHandler_t settings_x = sista_createANSISettings(
+        F_RED,
+        B_BLACK,
+        A_FAINT
+    );
+    if (settings_x == nullptr) {
+        sista_destroySwappableField(field);
+        return 1; // Could not allocate memory for the settings
+    }
     PawnHandler_t pawn_x = sista_createPawnInSwappableField(
         field, 'X',
-        sista_createANSISettings(
-            F_RED,
-            B_BLACK,
-            A_FAINT
-        ),
+        settings_x,
         {4, 5}
     );
     if (pawn_x == nullptr) {
+        sista_destroyANSISettings(settings_x);
+        sista_destroySwappableField(field);
         return 1; // Could not allocate memory for the pawn
+    }
+    ANSISettingsHandler_t settings_o = sista_createANSISettings(
+        F_GREEN,
+        B_BLACK,
+        A_UNDERLINE
+    );
+    if (settings_o == nullptr) {
+        sista_destroyANSISettings(settings_x);
+        sista_destroySwappableField(field);
+        return 1; // Could not allocate memory for the settings
     }
     PawnHandler_t pawn_o = sista_createPawnInSwappableField(
         field, 'O',
-        sista_createANSISettings(
-            F_GREEN,
-            B_BLACK,
-            A_UNDERLINE
-        ),
+        settings_o,
         {5, 5}
     );
     if (pawn_o == nullptr) {
+        sista_destroyANSISettings(settings_o);
+        sista_destroyANSISettings(settings_x);
+        sista_destroySwappableField(field);
         return 1; // Could not allocate memory for the pawn
     }
-    BorderHandler_t border = sista_createBorder(
-        '#', sista_createANSISettings(
-            F_WHITE,
-            B_BLACK,
-            A_BLINK
-        )
+    ANSISettingsHandler_t settings_border = sista_createANSISettings(
+        F_WHITE,
+        B_BLACK,
+        A_BLINK
     );
+    if (settings_border == nullptr) {
+        sista_destroyANSISettings(settings_o);
+        sista_destroyANSISettings(settings_x);
+        sista_destroySwappableField(field);
+        return 1; // Could not allocate memory for the settings
+    }
+    BorderHandler_t border = sista_createBorder(
+        '#', settings_border
+    );
+    if (border == nullptr) {
+        sista_destroyANSISettings(settings_o);
+        sista_destroyANSISettings(settings_x);
+        sista_destroySwappableField(field);
+        return 1; // Could not allocate memory for the border
+    }
     sista_printSwappableFieldWithBorder(field, border);
     std::flush(std::cout);
     std::this_thread::sleep_for(std::chrono::seconds(2));
@@ -50,7 +82,10 @@ int main() {
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
     sista_resetAnsi();
+    sista_destroyANSISettings(settings_o);
+    sista_destroyANSISettings(settings_x);
     sista_destroyBorder(border);
+    sista_destroyANSISettings(settings_border);
     sista_destroySwappableField(field);
     return 0;
 }
