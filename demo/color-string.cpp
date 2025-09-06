532895eeb982b2b4f7e23f7c701cd7a05db7fc41
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2025-09-06 21:56:08 +0300

    New demo better resetting style

--- demo/color-string.cpp
+++ demo/color-string.cpp
@@ -5,7 +5,8 @@ int main() {
     std::cout << sista::fgColorStr(sista::ForegroundColor::RED) << "This is red text" << sista::fgColorStr(sista::ForegroundColor::WHITE) << std::endl;
     std::string bgBlue = sista::bgColorStr(sista::BackgroundColor::BLUE);
     std::string attrBright = sista::attrStr(sista::Attribute::BRIGHT);
-    std::cout << bgBlue << attrBright << "This is bright text on blue background" << std::endl;
+    std::cout << bgBlue << attrBright << "This is bright text on blue background";
     sista::resetAnsi();
+    std::cout << " - back to normal" << std::endl;
     return 0;
 }
