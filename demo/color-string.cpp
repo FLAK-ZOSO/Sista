840638e54600882743c378b1d93f70b10df3ac29
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2025-09-06 21:49:31 +0300

    Added new demo for testing *Str() functions

---
+++ demo/color-string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/sista/sista.hpp"
+
+int main() {
+    std::cout << sista::fgColorStr(sista::ForegroundColor::RED) << "This is red text" << sista::fgColorStr(sista::ForegroundColor::WHITE) << std::endl;
+    std::string bgBlue = sista::bgColorStr(sista::BackgroundColor::BLUE);
+    std::string attrBright = sista::attrStr(sista::Attribute::BRIGHT);
+    std::cout << bgBlue << attrBright << "This is bright text on blue background" << std::endl;
+    sista::resetAnsi();
+    return 0;
+}
