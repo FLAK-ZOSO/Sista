e501c0ed8a3decba612b8c15b8ea17861f1ed889
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2025-12-21 23:55:41 +0200

    Less redundancy in the setup.py with is_windows

--- python/setup.py
+++ python/setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, Extension
 import platform
 
+is_windows = platform.system() == "Windows"
+
 sista_ext = Extension(
     "sista._sista",
     sources=["src/sistamodule.c"],
     include_dirs=[
         "/usr/local/include", 
         "/usr/include"
-    ] if not platform.platform().lower().startswith("win") else [],
+    ] if not is_windows else [],
     library_dirs=[
         "/usr/local/lib", 
         "/usr/lib"
-    ] if not platform.platform().lower().startswith("win") else [],
+    ] if not is_windows else [],
     language="c",
     libraries=["Sista_api"],
     extra_link_args=[
         "-Wl,-rpath,/usr/local/lib",
         "-Wl,-rpath,/usr/lib"
-    ] if not platform.platform().lower().startswith("win") else []
+    ] if not is_windows else []
 )
 
 # Only provide build-time things here (extensions). Project metadata lives in pyproject.toml
