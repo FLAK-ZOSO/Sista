50dd65567c4bfbaabdb9a67c1c9f901adb506093
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2026-01-08 12:57:40 +0200

    Version bump for release

--- python/src/sista/__init__.pyi
+++ python/src/sista/__init__.pyi
@@ -5,7 +5,7 @@ from typing import Any
 # Type alias used for opaque Python capsules returned by the C extension.
 Capsule = Any
 
-__version__ = '3.0.0-beta.5'
+__version__ = '3.0.0-beta.6'
 
 # constants
 F_BLACK: int
